3d12b3163742773c57c95db390be6bb7ecacd800
DavvveN <david@sunnebo.se>
2022-09-03 12:18:36 +0200

    Triangle Area

---
+++ TriangelArea/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+
+        int a = scr.nextInt();
+        int b = scr.nextInt();
+
+        System.out.println((double)(a*b)/2);
+    }
+}
