8a825a2cd79af7092e28f03497862d6230c6257d
DavvveN <david@sunnebo.se>
2022-09-03 12:18:25 +0200

    Stuck In a Time Loop

---
+++ StuckInATimeLoop/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int i = scanner.nextInt();
+
+        for(int l = 1; l < i+1; l++){
+            System.out.println(l+" Abracadabra");
+        }
+    }
+}
