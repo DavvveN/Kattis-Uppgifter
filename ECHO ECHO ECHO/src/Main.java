b843142587a5ce5a24c6a49f708dd51ac733194e
David Nilsson <65786975+DavvveN@users.noreply.github.com>
2022-09-14 14:09:19 +0200

    ECHO ECHO ECHO

---
+++ ECHO ECHO ECHO/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        String word = scr.nextLine();
+
+        System.out.println(word + " "+ word + " "+ word);
+    }
+}
